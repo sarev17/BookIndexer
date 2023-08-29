c68478617808341213c97e2c5d543a2b5795d61c
sarev17 <sarev17@gmail.com>
2023-08-29 20:25:44 -0300

    feat: get user token

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function getToken(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken();
+            $user->update(['api_token'=>$token]);
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'api_token'
     ];
 
     /**
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function createToken(){
+        return hash('sha256', random_int(10, 100));
+    }
 }
